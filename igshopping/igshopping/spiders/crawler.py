0787a95895fc684112fcd369af9f7ad08210a758
Leandro <leandro.costa@pxcreativelab.com>
2026-04-28 16:45:01 -0300

    atualização para utilizar o chrome

--- igshopping/igshopping/spiders/crawler.py
+++ igshopping/igshopping/spiders/crawler.py
@@ -34,7 +34,7 @@ class IgshoppingcrawlerSpider(scrapy.Spider):
         if self.headless:
             options.add_argument("--headless=new")
         
-        options.add_argument("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0")
+        options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
         
         self.driver = webdriver.Chrome(options=options)
         self.driver.set_window_size(1920, 1080)
